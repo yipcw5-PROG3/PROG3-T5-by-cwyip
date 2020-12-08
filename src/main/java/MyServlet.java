67d0c932e653a1492075552d335323288c8d83c6
yipcw5 <yipcw9@gmail.com>
2020-12-08 14:36:46 +0000

    Ex5

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -4,16 +4,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello, world!");
+        resp.getWriter().write("<p>Hello, world!</p>");
         req.getServletPath(); // returns the ServletPath of the URL
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        resp.setContentType("text/html");
+        resp.getWriter().write("Thank you client! "+reqBody);
     }
 }
